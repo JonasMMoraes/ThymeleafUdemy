4be426eb97b18066ae42df484253a169b24d593b
Jonas Moreno de Moraes <jonas.moraes@comlink.com.br>
2023-04-20 17:43:46 -0300

    mais um commit

--- src/main/java/com/example/thymeleafudemy/service/DepartamentoServiceImpl.java
+++ src/main/java/com/example/thymeleafudemy/service/DepartamentoServiceImpl.java
@@ -1,44 +1,50 @@
-//package com.example.thymeleafudemy.service;
-//
-//import com.example.thymeleafudemy.dao.DepartamentoDao;
-//import com.example.thymeleafudemy.domain.Departamento;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//public class DepartamentoServiceImpl implements DepartamentoService {
-//
-//    @Autowired
-//    private DepartamentoDao dao;
-//
-//    @Override
-//    public void salvar(Departamento departamento) {
-//        dao.save(departamento);
-//    }
-//
-//    @Override
-//    public void editar(Departamento departamento) {
-//        dao.update(departamento);
-//    }
-//
-//    @Override
-//    public void excluir(Long id) {
-//        dao.delete(id);
-//    }
-//
-//    @Override
-//    @Transactional(readOnly = true)
-//    public Departamento buscarPorId(Long id) {
-//        return dao.findById(id);
-//    }
-//
-//    @Override
-//    @Transactional(readOnly = true)
-//    public List<Departamento> buscarTodos() {
-//        return dao.findAll();
-//    }
-//}
+package com.example.thymeleafudemy.service;
+
+import com.example.thymeleafudemy.dao.DepartamentoDao;
+import com.example.thymeleafudemy.domain.Departamento;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.NamedNativeQuery;
+import java.util.List;
+
+@Service
+@Transactional
+public class DepartamentoServiceImpl implements DepartamentoService {
+
+    @Autowired
+    private DepartamentoDao dao;
+
+    @Override
+    public void salvar(Departamento departamento) {
+        dao.save(departamento);
+    }
+
+    @Override
+    public void editar(Departamento departamento) {
+        dao.update(departamento);
+    }
+
+    @Override
+    public void excluir(Long id) {
+        dao.delete(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Departamento buscarPorId(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Departamento> buscarTodos() {
+        return dao.findAll();
+    }
+
+    @Override
+    public boolean departamentoTemCargos(Long id) {
+        return !buscarPorId(id).getCargos().isEmpty();
+    }
+}
--- src/main/java/com/example/thymeleafudemy/web/controller/DepartamentoController.java
+++ src/main/java/com/example/thymeleafudemy/web/controller/DepartamentoController.java
@@ -1,21 +1,61 @@
 package com.example.thymeleafudemy.web.controller;
 
+import com.example.thymeleafudemy.domain.Departamento;
+import com.example.thymeleafudemy.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    DepartamentoService departamentoService;
+
     @GetMapping("/cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Departamento departamento) {
         return "/departamento/cadastro";
     }
 
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento) {
+        departamentoService.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
     @GetMapping("/listar")
-    public String listar() {
+    public String listar(ModelMap modelMap) {
+        List<Departamento> departamentos = departamentoService.buscarTodos();
+        modelMap.addAttribute("departamentos", departamentos);
         return "/departamento/lista";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap modelMap) {
+        modelMap.addAttribute("departamento", departamentoService.buscarPorId(id));
+        return "/departamento/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Departamento departamento) {
+        departamentoService.editar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
+    @GetMapping("/excluir/{ìd}")
+    public String excluir(@PathVariable("id") Long id, ModelMap modelMap) {
+        if (!departamentoService.departamentoTemCargos(id)) {
+            departamentoService.excluir(id);
+        }
+        return listar(modelMap);
     }
 
 }
